089bd34c4832d47a019606fc3028ad7a73a1b18e
Ruslan Abzanov <dessu.dess@gmail.com>
2022-02-24 21:44:57 +0300

    ProfileView добавлен на сториборд, а не программно

--- Netology_IB_Instruments/ProfileViewController.swift
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -11,15 +11,7 @@ class ProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        guard let profileView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? UIView else {return}
-
-        self.view.addSubview(profileView)
-
-        profileView.translatesAutoresizingMaskIntoConstraints = true
-        profileView.center = CGPoint(x: view.bounds.midX, y: view.bounds.midY)
-        profileView.autoresizingMask = [UIView.AutoresizingMask.flexibleLeftMargin, UIView.AutoresizingMask.flexibleRightMargin, UIView.AutoresizingMask.flexibleTopMargin, UIView.AutoresizingMask.flexibleBottomMargin]
-        
+    
     }
     
 }
